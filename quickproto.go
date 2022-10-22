613b526bdd1824cb9b4cc097ab5d7d73480b6e6a
Nigel van Keulen <Nigelvk@itextra.nl>
2022-10-22 14:17:05 +0200

    Added banned delimiter

--- quickproto.go
+++ quickproto.go
@@ -26,7 +26,7 @@ import (
 
 var STANDARD_DELIM []byte = []byte("$")
 var BANNED_DELIMITERS = []string{
-	"=",
+	"=", "_",
 	"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
 	"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
 }
