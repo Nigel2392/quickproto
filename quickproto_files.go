6c924e71770922ab2dcc0fcf6e4ee25c74122ac2
Nigel van Keulen <Nigelvk@itextra.nl>
2022-10-23 21:48:08 +0200

    Added methods for MessageFile

--- quickproto_files.go
+++ quickproto_files.go
@@ -1,7 +1,35 @@
 package quickproto
 
+import (
+	"os"
+	"path/filepath"
+)
+
 // Message file used for storing files sent inside the Message struct
 type MessageFile struct {
 	Name string
 	Data []byte
+}
+
+// NewMessageFile creates a new MessageFile.
+func NewMessageFile(name string, data []byte) MessageFile {
+	return MessageFile{
+		Name: name,
+		Data: data,
+	}
+}
+
+// Size returns the size of the file.
+func (f *MessageFile) Size() int {
+	return len(f.Data)
+}
+
+// String returns the name of the file.
+func (f *MessageFile) String() string {
+	return f.Name
+}
+
+// Save the file to a path.
+func (f *MessageFile) Save(path string) error {
+	return os.WriteFile(filepath.Join(path, f.Name), f.Data, 0644)
 }
